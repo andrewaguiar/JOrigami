45c5d622e0f39ba512c87d0cb19e6b953556c6b9
Andrew S Aguiar <andrewaguiar6@gmail.com>
2012-03-04 13:52:09 -0300

    testing jenkins

--- tests/CustomizedNameFormatter.java
+++ tests/CustomizedNameFormatter.java
@@ -1,9 +1,9 @@
-import br.org.origami.OrigamiFormater;
+import br.org.origami.DefaultOrigamiFormatter;
 
-public class CustomizedNameFormatter implements OrigamiFormater {
+public class CustomizedNameFormatter extends DefaultOrigamiFormatter {
 
 	@Override
 	public Object convert(final String value, final Class transformType, final String sub, final String opt) throws Exception {
-		return "---" + value.toUpperCase() + "---";
+		return "---" + super.convert(value, transformType, sub, opt).toString().toUpperCase() + "---";
 	}
 }
